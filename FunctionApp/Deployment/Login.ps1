15ad978f803f759788b5768f30d4eaa81bd440f8
Haley Loyd <hloyd@atlasrfid.com>
2019-01-28 10:03:36 -0600

    first commit

---
+++ FunctionApp/Deployment/Login.ps1
@@ -0,0 +1,13 @@
+﻿$username = "deployUser"
+$password = ConvertTo-SecureString -String "rfid123!" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
+$subscription = "0bb50060-a34f-4f2c-a36f-21836bf6d0e5"
+$tenant = "629fea65-2751-4995-acb4-850c6670f000"
+
+
+Connect-AzureRmAccount -credential $Credential -subscription $subscription -Force
+
+
+$Credential = Get-Credential
+
+Connect-AzureRmAccount -Credential $Credential -Tenant "xxxx-xxxx-xxxx-xxxx" -ServicePrincipal
---
+++ FunctionApp/Deployment/TextFile1.ps1
@@ -0,0 +1,13 @@
+﻿
+$siteName = 'FunctionApp-test-hl'
+$ResourceGroupName = 'function-app-test'
+$PublishingProfile = [xml](Get-AzureRmWebAppPublishingProfile -ResourceGroupName $ResourceGroupName -Name $siteName)
+
+$Username = (Select-Xml -Xml $PublishingProfile -XPath "//publishData/publishProfile[contains(@profileName,'Web Deploy')]/@userName").Node.Value
+$Password = (Select-Xml -Xml $PublishingProfile -XPath "//publishData/publishProfile[contains(@profileName,'Web Deploy')]/@userPWD").Node.Value
+
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username,$Password)))
+
+$apiUrl = "https://$siteName.scm.azurewebsites.net/api/zipdeploy";
+
+Invoke-RestMethod -Uri $apiUrl -InFile 'D:\site.zip' -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Post -ContentType "multipart/form-date";
